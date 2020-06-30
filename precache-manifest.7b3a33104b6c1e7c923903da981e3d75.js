87c4141a18bc69baac8ac5de341f79d3635f21aa
chelingliao <jessie910916@gmail.com>
2020-06-30 17:58:37 +0800

    Updates

---
+++ precache-manifest.7b3a33104b6c1e7c923903da981e3d75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e7dfbd360135ebfd6ee7129d2217eb",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "808f8c4132bf5cd3f77b",
+    "url": "/to-do-list/static/css/main.939021b3.chunk.css"
+  },
+  {
+    "revision": "865b65f1a7491b74b7b9",
+    "url": "/to-do-list/static/js/2.b8985b74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.b8985b74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808f8c4132bf5cd3f77b",
+    "url": "/to-do-list/static/js/main.a35178cc.chunk.js"
+  },
+  {
+    "revision": "b921f263160d338d2b82",
+    "url": "/to-do-list/static/js/runtime-main.53cf53c1.js"
+  }
+]);
